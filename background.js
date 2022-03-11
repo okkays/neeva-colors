c2bab8232613cac4c1ebdf81bc4ebde46f58fb1f
Kevin Kays <kkays@raybeam.com>
2022-03-11 09:29:03 -0500

    fix the settings switcher spontaneously stopping working

--- background.js
+++ background.js
@@ -1,34 +1,41 @@
-var currentScheme = null;
-var tabs = [];
-
-function applyScheme({neevaScheme, tabId}) {
-  if (currentScheme) chrome.scripting.removeCSS(currentScheme);
-  if (!neevaScheme || neevaScheme === 'default') {
-    return;
-  };
-
-  currentScheme = {
+function getCss(neevaScheme, {tabId}) {
+  return {
     files: [
       'mapper.css',
       `themes/${neevaScheme}.css`,
     ],
     target: {tabId},
   };
-  chrome.scripting.insertCSS(currentScheme);
+}
+
+function applyScheme({oldScheme, newScheme, tabId}) {
+  if (oldScheme) {
+    oldCss = getCss(oldScheme, {tabId});
+    chrome.scripting.removeCSS(oldCss);
+  }
+  if (!newScheme || newScheme === 'default') return;
+
+  newCss = getCss(newScheme, {tabId});
+  chrome.scripting.insertCSS(newCss);
 }
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area !== 'sync') return;
   if (!changes.neevaScheme?.newValue) return;
-  for (const tabId of tabs) {
-    applyScheme({neevaScheme: changes.neevaScheme.newValue, tabId});
-  }
+  chrome.tabs.query({url: ['*://neeva.com/*']}, (tabs) => {
+    for (const tab of tabs) {
+      applyScheme({
+        oldScheme: changes.neevaScheme.oldValue,
+        newScheme: changes.neevaScheme.newValue,
+        tabId: tab.id,
+      });
+    }
+  });
 });
 
 chrome.webNavigation.onCommitted.addListener(function(details) {
   chrome.storage.sync.get(['neevaScheme'], ({neevaScheme}) => {
-    applyScheme({neevaScheme, tabId: details.tabId});
-    tabs.push(details.tabId);
+    applyScheme({newScheme: neevaScheme, tabId: details.tabId});
   });
 }, {
   url: [{hostContains: 'neeva.com'}],
