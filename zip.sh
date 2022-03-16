055c3752e82f166b01aa6d14dd6400427e000002
Kevin Kays <kkays@raybeam.com>
2022-03-16 19:27:57 -0400

    fix zipping to include settings

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f build.zip
-zip -r build.zip background.js images/neeva-*.png manifest.json mapper.css themes/*.css
+zip -r build.zip insertSetting.js background.js images/neeva-*.png manifest.json mapper.css themes/*.css
